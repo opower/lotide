e080050a35315708375f1dcad479cfacfc3afe5a
Olivia <oliviapower.02@gmail.com>
2020-01-08 02:33:22 +0000

    adding findKeyByValue

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+const assertEqual = (actual, expected) => {
+
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} != ${expected}`);
+  }
+
+};
+
+const findKeyByValue = (shows, value) =>{
+
+  let names = Object.values(shows);
+  let keys = Object.keys(shows);
+
+  for(const i in names){
+    if(names[i] === value){
+      return keys[i];
+    }
+  }
+
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
