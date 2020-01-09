8dfd29f9e3bcfdc4f2385e8e3ce73bb558b9127e
Olivia <oliviapower.02@gmail.com>
2020-01-09 21:31:34 +0000

    adding findKey.js

---
+++ findKey.js
@@ -0,0 +1,42 @@
+const assertEqual = (actual, expected) => {
+
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} != ${expected}`);
+  }
+
+};
+
+const findKey = (obj, callback) => {
+
+  let keys = Object.keys(obj);
+  for (const j of keys) {
+    if (callback(obj[j])) {
+      return j;
+    }
+  }
+  return undefined;
+};
+
+let result = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+
+assertEqual(result, 'noma');
+
+let result1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 4); // => "noma"
+
+assertEqual(result1, undefined);
