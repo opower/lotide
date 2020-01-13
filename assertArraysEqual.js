4bbb79c78b9c03d531e0ad8389a1124841a55570
Olivia <oliviapower.02@gmail.com>
2020-01-13 19:12:22 +0000

    adding assertArraysEqualTest

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,4 @@
-const eqArrays = (a1, a2) => {
-
-  if(a1.length != a2.length){
-    return false;
-  }
-  for(const i in a1){
-    if(a1[i] != a2[i]){
-      return false;
-    }
-  }
-  return true;
-
-}
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = (a1, a2) => {
 
@@ -21,4 +9,4 @@ const assertArraysEqual = (a1, a2) => {
   }
 }
 
-assertArraysEqual([1,2,3], [1,2]);
+module.exports = assertArraysEqual;
