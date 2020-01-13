b2cdb5208b4d2a1c920c64fea242bbbdd04239af
Olivia <oliviapower.02@gmail.com>
2020-01-13 19:17:32 +0000

    adding middleTest

--- middle.js
+++ middle.js
@@ -1,26 +1,5 @@
-const eqArrays = (a1, a2) => {
-
-  if(a1.length != a2.length){
-    return false;
-  }
-  else{
-    for(let i = 0; i < a1.length; i++){
-      if(a1[i] != a2[i]){
-        return false;
-      }
-    }
-    return true;
-  }
-}
-
-const assertArraysEqual = (a1, a2) => {
-
-  if(eqArrays(a1,a2)){
-    console.log(`✅✅✅ Assertion Passed: ${a1} === ${a2}`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed: ${a1} != ${a2}`);
-  }
-}
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
 const middle = arr => {
 
@@ -45,6 +24,4 @@ const middle = arr => {
   }
   
 }
-
-
-assertArraysEqual(middle([1,2,3]),[3]);
+module.exports = middle;
